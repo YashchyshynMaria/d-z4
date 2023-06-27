8a68ce57c34edfdc92222255370aa38712b694c0
YashchyshynMaria <yashchishin1997@gmail.com>
2023-06-27 14:09:59 +0300

    commit

---
+++ src/main/java/IntegersMethod.java
@@ -0,0 +1,8 @@
+public class IntegersMethod {
+    int a = 10;
+    public String writeA() {
+        String lineForPrint = ("---"+a+"---");
+        System.out.print(lineForPrint);
+        return lineForPrint;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+
+        IntegersMethod object = new IntegersMethod();
+        object.writeA();
+        System.out.printf("\"~~~<%s>~~~\"%n",object.writeA());
+    }
+}
+
+
